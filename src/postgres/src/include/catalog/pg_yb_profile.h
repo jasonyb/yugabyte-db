660724830520e8a4b943cc4271189c0ec5faa0e3
Jason Kim <git@jasonk.me>
2022-12-16 20:24:50 -0800

    style: formatting pg_yb_profile.h

--- src/postgres/src/include/catalog/pg_yb_profile.h
+++ src/postgres/src/include/catalog/pg_yb_profile.h
@@ -27,9 +27,9 @@
  */
 CATALOG(pg_yb_profile,8051,YbProfileRelationId) BKI_SHARED_RELATION BKI_ROWTYPE_OID(8053,YbProfileRelation_Rowtype_Id) BKI_SCHEMA_MACRO
 {
-	NameData	prfname;		/* profile name */
-	int32		prfmaxfailedloginattempts;		/* no. of attempts allowed */
-	int32		prfpasswordlocktime;  /* secs to lock out an account */
+	NameData	prfname;					/* profile name */
+	int32		prfmaxfailedloginattempts;	/* no. of attempts allowed */
+	int32		prfpasswordlocktime;		/* secs to lock out an account */
 } FormData_pg_yb_profile;
 
 /* ----------------
